34c8fc533aa4a2cb8d23119472ef5d3b681b624f
Collins Mwendwa <collinsmwendwa1968@gmail.com>
2023-09-14 14:26:40 +0300

    Add project files.

---
+++ Models/AddCarDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppCars.Models
+{
+    public class AddCarDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string Color { get; set; } = string.Empty;
+
+        [Required]
+        public string Image { get; set; } = string.Empty;
+
+        [Required]
+        public int Price { get; set; }
+
+        public List<Make> Makes { get; set; } = new List<Make>();
+
+    }
+
+    public class Make
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ModelName { get; set; }
+    }
+}
---
+++ Models/CarDto.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorAppCars.Models
+{
+    public class CarDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Color { get; set; } = string.Empty;
+
+        public string Image { get; set; } = string.Empty;
+
+        public int Price { get; set; }
+    }
+}
